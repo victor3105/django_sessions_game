bf6c1d81df0c3e1c6236a927abc8313415aa0fb7
Victor Glebov <glebovv3105@gmail.com>
2020-10-27 22:08:33 -0600

    Added fields to models

--- game/models.py
+++ game/models.py
@@ -2,12 +2,18 @@ from django.db import models
 
 
 class Player(models.Model):
-    pass
+    def __str__(self):
+        return f'Player {id}'
 
 
 class Game(models.Model):
-    pass
+    is_finished = models.BooleanField()
+    players = models.ManyToManyField(Player, through='PlayerGameInfo', blank=True, null=True)
+
+    def __str__(self):
+        return f'Game {id}'
 
 
 class PlayerGameInfo(models.Model):
-    pass
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
--- game/views.py
+++ game/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Player, Game
 
 
 def show_home(request):
+    # Check if there are existing games
+
+
     return render(
         request,
         'home.html'
